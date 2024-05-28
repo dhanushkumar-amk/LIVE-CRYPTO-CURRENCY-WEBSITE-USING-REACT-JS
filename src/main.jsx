dcef9bd7f9bf615c47d4f281dca78f47c4ec64c6
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-05-28 12:00:31 +0530

    uploaded

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom';
+import CoinContextProvider from './context/coinContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CoinContextProvider>
+        <App />
+      </CoinContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,13 @@
-import './Home.css';
+import {useContext, useEffect, useState} from 'react';import './Home.css';
+import {CoinContext} from '../../context/coinContext';
 const Home = () => {
+  const {allCoin, currency} = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin]);
+
   return (
     <div className='home'>
       <div className='hero'>
@@ -7,7 +15,7 @@ const Home = () => {
           Largest <br /> Crypto MarketPlace{' '}
         </h1>
         <p>
-          Welcome to the world's largest cryptocurrency marketPlace. sign up to
+          Welcome to the worlds largest cryptocurrency marketPlace. sign up to
           explore more about cryptos
         </p>
         <form>
@@ -27,6 +35,34 @@ const Home = () => {
           <p style={{textAlign: 'center'}}>24h Change</p>
           <p className='market-cap'>Market Cap</p>
         </div>
+        {displayCoin.slice(0, 10).map((item, index) => (
+          <div
+            className='table-layout'
+            key={index}>
+            <p>{item.market_cap_rank} </p>
+            <div>
+              <img
+                src={item.image}
+                alt='coin image'
+              />
+              <p>{item.name + ' - ' + item.symbol}</p>
+            </div>
+
+            <p>
+              {currency.symbol} {item.current_price.toLocaleString()}{' '}
+            </p>
+            <p
+              className={
+                item.price_change_percentage_24h > 0 ? 'green' : 'red'
+              }>
+              {Math.floor(item.price_change_percentage_24h * 100) / 100}
+            </p>
+            <p className='market-cap'>
+              {' '}
+              {currency.symbol} {item.market_cap.toLocaleString()}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
