fc727a44b8e7ee6222153d32a4b3ac5e641da8eb
Dhanushkumar G <145154541+dhanushkumar-amk@users.noreply.github.com>
2024-06-27 11:18:07 +0530

    Update Coin.jsx

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -35,7 +35,7 @@ const Coin = () => {
     };
 
     fetch(
-      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`,
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=5&interval=daily`,
       options
     )
       .then((response) => response.json())
